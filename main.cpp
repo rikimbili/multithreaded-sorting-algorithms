fa4e643f7ddf0a8216bdfd1908977bd7ed70268d
Kevin <kevfonso@gmail.com>
2023-02-25 22:37:45 -0500

    Separate merge from merge sort, Implement concurrent merge sort and benchmarking, Trying to figure out concurrent bubble sort merge

--- main.cpp
+++ main.cpp
@@ -22,15 +22,33 @@ int *getArrCopy(const int arr[]) {
     return arr_copy;
 }
 
+// Mainly for debugging purposes
+void isSorted(int arr[], string arrName = "Array") {
+    for (int i = 0; i < ARR_SIZE - 1; i++) {
+        if (arr[i] > arr[i + 1]) {
+            cout << arrName << " is not sorted" << endl;
+            cout << "arr[" << i << "] = " << arr[i] << " and arr[" << i + 1 << "] = " << arr[i + 1] << endl;
+            return;
+        }
+    }
+    cout << arrName << " is sorted" << endl;
+}
+
 long getBubbleSortTime(int arr[], int num_threads) {
     auto arr_copy = getArrCopy(arr);
+    isSorted(arr_copy, "Original array");
     auto t1 = chrono::high_resolution_clock::now();
-    if (num_threads == 1) { bubbleSort(arr_copy, 0, ARR_SIZE - 1); }
-    else { concurrentBubbleSort(arr_copy, ARR_SIZE, num_threads); }
+    if (num_threads == 1) { 
+        bubbleSort(arr_copy, 0, ARR_SIZE - 1);
+        isSorted(arr_copy, "Original array with 1 thread");
+    }
+    else { 
+        concurrentBubbleSort(arr_copy, ARR_SIZE, num_threads);
+        isSorted(arr_copy, "Original array with " + to_string(num_threads) + " threads");
+    }
     delete arr_copy;
     auto t2 = chrono::high_resolution_clock::now();
     return chrono::duration_cast<chrono::milliseconds>(t2 - t1).count();
-
 }
 
 void benchmarkBubbleSort(int arr[]) {
@@ -43,6 +61,43 @@ void benchmarkBubbleSort(int arr[]) {
     auto concurrent_time_8 = getBubbleSortTime(arr, 8);
     
     vt.addRow("Bubble Sort", to_string(non_concurrent_time) + "ms",
+              to_string(concurrent_time_2) + "ms (" +
+              to_string((concurrent_time_2 - non_concurrent_time) * 100 / non_concurrent_time) + "%)",
+              to_string(concurrent_time_4) + "ms (" +
+              to_string((concurrent_time_4 - non_concurrent_time) * 100 / non_concurrent_time) + "%)",
+              to_string(concurrent_time_8) + "ms (" +
+              to_string((concurrent_time_8 - non_concurrent_time) * 100 / non_concurrent_time) + "%)");
+
+    spinner->stop();
+}
+
+long getMergeSortTime(int arr[], int num_threads) {
+    auto arr_copy = getArrCopy(arr);
+    isSorted(arr_copy, "Original array");
+    auto t1 = chrono::high_resolution_clock::now();
+    if (num_threads == 1) { 
+        mergeSort(arr_copy, 0, ARR_SIZE - 1); 
+        isSorted(arr_copy, "Original array with 1 thread");
+    }
+    else { 
+        concurrentMergeSort(arr_copy, 0, ARR_SIZE - 1, num_threads);
+        isSorted(arr_copy, "Original array with " + to_string(num_threads) + " threads"); 
+    }
+    delete arr_copy;
+    auto t2 = chrono::high_resolution_clock::now();
+    return chrono::duration_cast<chrono::milliseconds>(t2 - t1).count();
+}
+
+void benchmarkMergeSort(int arr[]) {
+    spinner->setText("Benchmarking merge sort...");
+    spinner->start();
+
+    auto non_concurrent_time = getMergeSortTime(arr, 1);
+    auto concurrent_time_2 = getMergeSortTime(arr, 2);
+    auto concurrent_time_4 = getMergeSortTime(arr, 4);
+    auto concurrent_time_8 = getMergeSortTime(arr, 8);
+    
+    vt.addRow("Merge Sort", to_string(non_concurrent_time) + "ms",
               to_string(concurrent_time_2) + "ms (" +
               to_string((concurrent_time_2 - non_concurrent_time) * 100 / non_concurrent_time) + "%)",
               to_string(concurrent_time_4) + "ms (" +
@@ -67,8 +122,8 @@ int main() {
         arr[i] = dis(gen);
     }
 
-
     benchmarkBubbleSort(arr);
+    benchmarkMergeSort(arr);
 
     vt.print(cout);
 
--- sorting_algorithms.hpp
+++ sorting_algorithms.hpp
@@ -6,39 +6,71 @@
 
 using namespace std;
 
+// Can be parallelized
+template <typename T>
+void merge(T arr[], int start, int mid, int end) {
+    int left = start;
+    int right = mid + 1;
+    int temp[end - start + 1];
+    int temp_index = 0;
+
+    while (left <= mid && right <= end) {
+        if (arr[left] < arr[right]) {
+            temp[temp_index] = arr[left];
+            left++;
+        } else {
+            temp[temp_index] = arr[right];
+            right++;
+        }
+        temp_index++;
+    }
+
+    while (left <= mid) {
+        temp[temp_index] = arr[left];
+        left++;
+        temp_index++;
+    }
+
+    while (right <= end) {
+        temp[temp_index] = arr[right];
+        right++;
+        temp_index++;
+    }
+
+    for (int i = start; i <= end; i++) {
+        arr[i] = temp[i - start];
+    }
+}
+
 template<class T>
 void mergeSort(T arr[], int start, int end) {
     if (start < end) {
         int mid = (start + end) / 2;
         mergeSort(arr, start, mid);
         mergeSort(arr, mid + 1, end);
-        int left = start;
-        int right = mid + 1;
-        int temp[end - start + 1];
-        int temp_index = 0;
-        while (left <= mid && right <= end) {
-            if (arr[left] < arr[right]) {
-                temp[temp_index] = arr[left];
-                left++;
-            } else {
-                temp[temp_index] = arr[right];
-                right++;
+        merge(arr, start, mid, end);
+    }
+}
+
+// Assumes that numThreads is a power of 2
+template <typename T>
+void concurrentMergeSort(T arr[], int start, int end, int numThreads) {
+    if (start < end) {
+        int mid = (start + end) / 2;
+        if (numThreads > 1) {
+            std::vector<std::thread> threads(2);
+            int numThreadsLeft = numThreads / 2;
+            int numThreadsRight = numThreads - numThreadsLeft;
+            threads[0] = std::thread(concurrentMergeSort<T>, arr, start, mid, numThreadsLeft);
+            threads[1] = std::thread(concurrentMergeSort<T>, arr, mid + 1, end, numThreadsRight);
+            for (auto& thread : threads) {
+                thread.join();
             }
-            temp_index++;
+        } else {
+            concurrentMergeSort(arr, start, mid, 1);
+            concurrentMergeSort(arr, mid + 1, end, 1);
         }
-        while (left <= mid) {
-            temp[temp_index] = arr[left];
-            left++;
-            temp_index++;
-        }
-        while (right <= end) {
-            temp[temp_index] = arr[right];
-            right++;
-            temp_index++;
-        }
-        for (int i = start; i <= end; i++) {
-            arr[i] = temp[i - start];
-        }
+        merge(arr, start, mid, end);
     }
 }
 
@@ -67,15 +99,68 @@ void concurrentBubbleSort(T arr[], int n, int num_threads) {
     for (int i = 0; i < num_threads; i++) {
         threads.emplace_back(bubbleSort<T>, ref(arr), start, end);
         start = end;
-        end += chunk_size;
+        end = min(end + chunk_size, n);
+        // end += chunk_size;
     }
+    // for (int i = 0; i < num_threads - 1; i++) {
+    // threads.emplace_back(bubbleSort<T>, ref(arr), i*chunk_size, (i+1)*chunk_size);
+    // }
+    // threads.emplace_back(bubbleSort<T>, ref(arr), (num_threads-1)*chunk_size, n);
 
     for (auto &thread: threads) {
         thread.join();
     }
 
-    // Merge the sorted chunks using merge sort
-    mergeSort(arr, 0, n - 1);
+    // Merge the sorted chunks using merge
+    // Start at i = 1 because the first chunk is already sorted
+    // Each iteration of the loop merges two adjacent chunks
+    // First chunk starts at 0 and ends at (i * chunk_size) - 1
+    // Second chunk starts at (i * chunk_size) and ends at (i + 1) * chunk_size - 1
+    for (int i = 1; i < num_threads; i++) {
+        // merge(arr, 0, (i * chunk_size) - 1, (i + 1) * chunk_size - 1);
+        int mid = i * chunk_size - 1;
+        int right = min((i + 1) * chunk_size - 1, n - 1);
+        merge(arr, 0, mid, right);
+    }
+    
+    // // Merge the sorted chunks using merge
+    // // Each iteration of the loop merges all adjacent chunks
+    // // The size of the chunks being merged doubles at each iteration
+    // for (int size = chunk_size; size < n; size *= 2) {
+    //     for (int i = 0; i < n; i += 2 * size) {
+    //         int left = i;
+    //         int mid = min(i + size - 1, n - 1);
+    //         int right = min(i + 2 * size - 1, n - 1);
+    //         merge(arr, left, mid, right);
+    //     }
+    // }
+
+    // // Merge the sorted chunks
+    // int merge_count = num_threads / 2;
+    // while (merge_count > 0) {
+    //     int chunk_start = 0;
+    //     for (int i = 0; i < merge_count; i++) {
+    //         int chunk_end = chunk_start + chunk_size * 2 - 1;
+    //         if (chunk_end >= n) {
+    //             chunk_end = n - 1;
+    //         }
+    //         merge(arr, chunk_start, chunk_start + chunk_size - 1, chunk_end);
+    //         chunk_start = chunk_end + 1;
+    //     }
+    //     if (merge_count == 1 && chunk_start < n) {
+    //         merge(arr, 0, chunk_start - 1, n - 1);
+    //     }
+    //     merge_count = merge_count / 2 + merge_count % 2;
+    // }
+    
+    // // merge sub-arrays iteratively
+    // for (int size = chunk_size; size < n; size *= 2) {
+    //     for (int left = 0; left < n - size; left += 2*size) {
+    //         int mid = left + size - 1;
+    //         int right = min(left + 2*size - 1, n - 1);
+    //         merge(arr, left, mid, right);
+    //     }
+    // }
 }
 
 #endif //SORTING_ALGORITHMS_HPP
