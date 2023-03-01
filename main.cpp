2662e713e601bf7c5fac9bc9a6e3132f694bd5a3
Kevin <kevfonso@gmail.com>
2023-03-01 17:10:32 -0500

    Fix concurrentBubbleSort, Add radixSort + concurrentRadixSort, small variable changes

--- main.cpp
+++ main.cpp
@@ -24,27 +24,29 @@ int *getArrCopy(const int arr[]) {
 
 // Mainly for debugging purposes
 void isSorted(int arr[], string arrName = "Array") {
+    bool isSorted = true;
     for (int i = 0; i < ARR_SIZE - 1; i++) {
         if (arr[i] > arr[i + 1]) {
+            isSorted = false;
             cout << arrName << " is not sorted" << endl;
-            cout << "arr[" << i << "] = " << arr[i] << " and arr[" << i + 1 << "] = " << arr[i + 1] << endl;
-            return;
+            cout << "arr[" << i << "] = " << arr[i] << " and arr[" << i + 1 << "] = " << arr[i + 1] << endl << endl;
+            // return;
         }
     }
-    cout << arrName << " is sorted" << endl;
+    if (isSorted) cout << arrName << " is sorted" << endl << endl;
 }
 
-long getBubbleSortTime(int arr[], int num_threads) {
+long getBubbleSortTime(int arr[], int numThreads) {
     auto arr_copy = getArrCopy(arr);
-    isSorted(arr_copy, "Original array");
+    // isSorted(arr_copy, "Original array");
     auto t1 = chrono::high_resolution_clock::now();
-    if (num_threads == 1) { 
-        bubbleSort(arr_copy, 0, ARR_SIZE - 1);
-        isSorted(arr_copy, "Original array with 1 thread");
+    if (numThreads == 1) { 
+        bubbleSort(arr_copy, 0, ARR_SIZE);
+        isSorted(arr_copy, "Array with 1 thread");
     }
     else { 
-        concurrentBubbleSort(arr_copy, ARR_SIZE, num_threads);
-        isSorted(arr_copy, "Original array with " + to_string(num_threads) + " threads");
+        concurrentBubbleSort(arr_copy, ARR_SIZE, numThreads);
+        isSorted(arr_copy, "Array with " + to_string(numThreads) + " threads");
     }
     delete arr_copy;
     auto t2 = chrono::high_resolution_clock::now();
@@ -55,6 +57,7 @@ void benchmarkBubbleSort(int arr[]) {
     spinner->setText("Benchmarking bubble sort...");
     spinner->start();
 
+    cout << "Benchmarking bubble sort..." << endl;
     auto non_concurrent_time = getBubbleSortTime(arr, 1);
     auto concurrent_time_2 = getBubbleSortTime(arr, 2);
     auto concurrent_time_4 = getBubbleSortTime(arr, 4);
@@ -71,17 +74,17 @@ void benchmarkBubbleSort(int arr[]) {
     spinner->stop();
 }
 
-long getMergeSortTime(int arr[], int num_threads) {
+long getMergeSortTime(int arr[], int numThreads) {
     auto arr_copy = getArrCopy(arr);
-    isSorted(arr_copy, "Original array");
+    // isSorted(arr_copy, "Original array");
     auto t1 = chrono::high_resolution_clock::now();
-    if (num_threads == 1) { 
+    if (numThreads == 1) { 
         mergeSort(arr_copy, 0, ARR_SIZE - 1); 
-        isSorted(arr_copy, "Original array with 1 thread");
+        isSorted(arr_copy, "Array with 1 thread");
     }
     else { 
-        concurrentMergeSort(arr_copy, 0, ARR_SIZE - 1, num_threads);
-        isSorted(arr_copy, "Original array with " + to_string(num_threads) + " threads"); 
+        concurrentMergeSort(arr_copy, 0, ARR_SIZE - 1, numThreads);
+        isSorted(arr_copy, "Array with " + to_string(numThreads) + " threads"); 
     }
     delete arr_copy;
     auto t2 = chrono::high_resolution_clock::now();
@@ -92,12 +95,51 @@ void benchmarkMergeSort(int arr[]) {
     spinner->setText("Benchmarking merge sort...");
     spinner->start();
 
+    cout << "Benchmarking merge sort..." << endl;
     auto non_concurrent_time = getMergeSortTime(arr, 1);
     auto concurrent_time_2 = getMergeSortTime(arr, 2);
     auto concurrent_time_4 = getMergeSortTime(arr, 4);
     auto concurrent_time_8 = getMergeSortTime(arr, 8);
     
     vt.addRow("Merge Sort", to_string(non_concurrent_time) + "ms",
+              to_string(concurrent_time_2) + "ms (" +
+              to_string((concurrent_time_2 - non_concurrent_time) * 100 / non_concurrent_time) + "%)",
+              to_string(concurrent_time_4) + "ms (" +
+              to_string((concurrent_time_4 - non_concurrent_time) * 100 / non_concurrent_time) + "%)",
+              to_string(concurrent_time_8) + "ms (" +
+              to_string((concurrent_time_8 - non_concurrent_time) * 100 / non_concurrent_time) + "%)");
+
+    spinner->stop();
+}
+
+long getRadixSortTime(int arr[], int numThreads) {
+    auto arr_copy = getArrCopy(arr);
+    // isSorted(arr_copy, "Original array");
+    auto t1 = chrono::high_resolution_clock::now();
+    if (numThreads == 1) { 
+        radixSort(arr_copy, 0, ARR_SIZE);
+        isSorted(arr_copy, "Array with 1 thread");
+    }
+    else { 
+        concurrentRadixSort(arr_copy, ARR_SIZE, numThreads);
+        isSorted(arr_copy, "Array with " + to_string(numThreads) + " threads");
+    }
+    delete arr_copy;
+    auto t2 = chrono::high_resolution_clock::now();
+    return chrono::duration_cast<chrono::milliseconds>(t2 - t1).count();
+}
+
+void benchmarkRadixSort(int arr[]) {
+    spinner->setText("Benchmarking radix sort...");
+    spinner->start();
+
+    cout << "Benchmarking radix sort..." << endl;
+    auto non_concurrent_time = getRadixSortTime(arr, 1);
+    auto concurrent_time_2 = getRadixSortTime(arr, 2);
+    auto concurrent_time_4 = getRadixSortTime(arr, 4);
+    auto concurrent_time_8 = getRadixSortTime(arr, 8);
+    
+    vt.addRow("Radix Sort", to_string(non_concurrent_time) + "ms",
               to_string(concurrent_time_2) + "ms (" +
               to_string((concurrent_time_2 - non_concurrent_time) * 100 / non_concurrent_time) + "%)",
               to_string(concurrent_time_4) + "ms (" +
@@ -124,6 +166,7 @@ int main() {
 
     benchmarkBubbleSort(arr);
     benchmarkMergeSort(arr);
+    benchmarkRadixSort(arr);
 
     vt.print(cout);
 
--- sorting_algorithms.hpp
+++ sorting_algorithms.hpp
@@ -57,12 +57,16 @@ template <typename T>
 void concurrentMergeSort(T arr[], int start, int end, int numThreads) {
     if (start < end) {
         int mid = (start + end) / 2;
+        
         if (numThreads > 1) {
-            std::vector<std::thread> threads(2);
+            vector<thread> threads(2);
+
             int numThreadsLeft = numThreads / 2;
             int numThreadsRight = numThreads - numThreadsLeft;
-            threads[0] = std::thread(concurrentMergeSort<T>, arr, start, mid, numThreadsLeft);
-            threads[1] = std::thread(concurrentMergeSort<T>, arr, mid + 1, end, numThreadsRight);
+
+            threads[0] = thread(concurrentMergeSort<T>, arr, start, mid, numThreadsLeft);
+            threads[1] = thread(concurrentMergeSort<T>, arr, mid + 1, end, numThreadsRight);
+            
             for (auto& thread : threads) {
                 thread.join();
             }
@@ -70,18 +74,18 @@ void concurrentMergeSort(T arr[], int start, int end, int numThreads) {
             concurrentMergeSort(arr, start, mid, 1);
             concurrentMergeSort(arr, mid + 1, end, 1);
         }
+
         merge(arr, start, mid, end);
     }
 }
 
 template<class T>
 void bubbleSort(T arr[], int start, int end) {
-    int n = end - start;
-    T temp;
+    int n = end - start + 1;
     for (int i = 0; i < n; i++) {
-        for (int j = start; j < end - i; j++) {
+        for (int j = start; j < end - i - 1; j++) {
             if (arr[j] > arr[j + 1]) {
-                temp = arr[j];
+                T temp = arr[j];
                 arr[j] = arr[j + 1];
                 arr[j + 1] = temp;
             }
@@ -90,77 +94,97 @@ void bubbleSort(T arr[], int start, int end) {
 }
 
 template<class T>
-void concurrentBubbleSort(T arr[], int n, int num_threads) {
+void concurrentBubbleSort(T arr[], int n, int numThreads) {
     vector<thread> threads;
-    int chunk_size = n / num_threads;
-    int start = 0;
-    int end = chunk_size;
+    int chunk_size = n / numThreads;
 
-    for (int i = 0; i < num_threads; i++) {
+    for (int i = 0; i < numThreads; i++) {
+        int start = i * chunk_size;
+        int end = min(start + chunk_size, n);
         threads.emplace_back(bubbleSort<T>, ref(arr), start, end);
-        start = end;
-        end = min(end + chunk_size, n);
-        // end += chunk_size;
     }
-    // for (int i = 0; i < num_threads - 1; i++) {
-    // threads.emplace_back(bubbleSort<T>, ref(arr), i*chunk_size, (i+1)*chunk_size);
-    // }
-    // threads.emplace_back(bubbleSort<T>, ref(arr), (num_threads-1)*chunk_size, n);
 
     for (auto &thread: threads) {
         thread.join();
     }
 
-    // Merge the sorted chunks using merge
-    // Start at i = 1 because the first chunk is already sorted
-    // Each iteration of the loop merges two adjacent chunks
-    // First chunk starts at 0 and ends at (i * chunk_size) - 1
-    // Second chunk starts at (i * chunk_size) and ends at (i + 1) * chunk_size - 1
-    for (int i = 1; i < num_threads; i++) {
-        // merge(arr, 0, (i * chunk_size) - 1, (i + 1) * chunk_size - 1);
-        int mid = i * chunk_size - 1;
-        int right = min((i + 1) * chunk_size - 1, n - 1);
-        merge(arr, 0, mid, right);
+    // Similar to merging in merge sort, but we merge chunks of size chunk_size
+    // Doubles the size of the chunks each iteration
+    for (int size = chunk_size; size < n; size *= 2) {
+        for (int i = 0; i < n - size; i += 2 * size) {
+            int left = i;
+            int mid = i + size - 1;
+            int right = min(i + 2 * size - 1, n - 1);
+            merge(arr, left, mid, right);
+        }
     }
-    
-    // // Merge the sorted chunks using merge
-    // // Each iteration of the loop merges all adjacent chunks
-    // // The size of the chunks being merged doubles at each iteration
-    // for (int size = chunk_size; size < n; size *= 2) {
-    //     for (int i = 0; i < n; i += 2 * size) {
-    //         int left = i;
-    //         int mid = min(i + size - 1, n - 1);
-    //         int right = min(i + 2 * size - 1, n - 1);
-    //         merge(arr, left, mid, right);
-    //     }
-    // }
+}
 
-    // // Merge the sorted chunks
-    // int merge_count = num_threads / 2;
-    // while (merge_count > 0) {
-    //     int chunk_start = 0;
-    //     for (int i = 0; i < merge_count; i++) {
-    //         int chunk_end = chunk_start + chunk_size * 2 - 1;
-    //         if (chunk_end >= n) {
-    //             chunk_end = n - 1;
-    //         }
-    //         merge(arr, chunk_start, chunk_start + chunk_size - 1, chunk_end);
-    //         chunk_start = chunk_end + 1;
-    //     }
-    //     if (merge_count == 1 && chunk_start < n) {
-    //         merge(arr, 0, chunk_start - 1, n - 1);
-    //     }
-    //     merge_count = merge_count / 2 + merge_count % 2;
-    // }
-    
-    // // merge sub-arrays iteratively
-    // for (int size = chunk_size; size < n; size *= 2) {
-    //     for (int left = 0; left < n - size; left += 2*size) {
-    //         int mid = left + size - 1;
-    //         int right = min(left + 2*size - 1, n - 1);
-    //         merge(arr, left, mid, right);
-    //     }
-    // }
+void radixSort(int arr[], int start, int end) {
+    // Get max element in array
+    int max = arr[start];
+    for (int i = start + 1; i <= end; i++) {
+        if (arr[i] > max) {
+            max = arr[i];
+        }
+    }
+
+    int n = end - start;
+    // Iterate through all digits of max element
+    for (int exp = 1; max / exp > 0; exp *= 10) {
+        int output[n];
+        int count[10] = {0};
+
+        // Count the number of elements with each digit
+        for (int i = start; i < end; i++) {
+            int digit = (arr[i] / exp) % 10;
+            count[digit]++;
+        }
+
+        // Cumulative sum of count array
+        for (int i = 1; i < 10; i++) {
+            count[i] += count[i - 1];
+        }
+
+        for (int i = end - 1; i >= start; i--) {
+            int digit = (arr[i] / exp) % 10;
+            output[count[digit] - 1] = arr[i];
+            count[digit]--;
+        }
+
+        // Copy output array to arr
+        for (int i = start; i < end; i++) {
+            arr[i] = output[i - start];
+        }
+    }
+}
+
+void concurrentRadixSort(int arr[], int n, int numThreads)
+{
+    vector<thread> threads;
+    int chunk_size = n / numThreads;
+
+    for (int i = 0; i < numThreads; i++) {
+        int start = i * chunk_size;
+        int end = min(start + chunk_size, n);
+        threads.emplace_back(radixSort, ref(arr), start, end);
+    }
+
+    for (auto &thread: threads) {
+        thread.join();
+    }
+
+    // Similar to merging in merge sort, but we merge chunks of size chunk_size
+    // Doubles the size of the chunks each iteration
+    for (int size = chunk_size; size < n; size *= 2) {
+        for (int i = 0; i < n - size; i += 2 * size) {
+            int left = i;
+            int mid = i + size - 1;
+            int right = min(i + 2 * size - 1, n - 1);
+            merge(arr, left, mid, right);
+        }
+    }
+
 }
 
 #endif //SORTING_ALGORITHMS_HPP
